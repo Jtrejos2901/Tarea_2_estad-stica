78c35b933e405437dda8864382eb29bed626bacf
Carolina <caronavamon25@gmail.com>
2024-02-04 22:41:28 -0600

    feat: Se corrige el ejercicio 2

--- Ejercicio 2.R
+++ Ejercicio 2.R
@@ -2,7 +2,6 @@
 
 #---Estimación de función de distribución mediante Muestreo por Importancia----
 
-
 set.seed(2901)
 n <- 10^4 #tamaño de la muestra
 
@@ -12,15 +11,23 @@ f <- dnorm(X, 0.5, sqrt(0.5))
 
 valor_estimado_1 <- mean(f) #La integración por Montecarlo Converge lento
 
+plot(density(X))
 
-#Se emplea como densidad auxiliar una exponencial truncada en 5 con lambda = 1
+#Necesitamos densidad auxiliar que contenga a la densidad de la normal(0.5,0.5)
 
-A <- rexp(n)+5 #datos aleatorios con distribución exponencial truncada
+#Se emplea como densidad auxiliar una exponencial truncada en 6 con lambda = 1
+
+A <- rexp(n)+6 #datos aleatorios con distribución exponencial truncada
 
 
-w <- dnorm(A, 0.5, sqrt(0.5)) / dexp(A-5)
+w <- dnorm(A, 0.5, sqrt(0.5)) / dexp(A-6)
 
 valor_estimado <- mean(w)
 
 valor_real <- pnorm(-5, 0.5, sqrt(0.5))
+ 
+error_absoluto <- abs(valor_estimado-valor_real)
+
+resumen <- data.frame("Estimación" = valor_estimado, "Valor real" = valor_real, "Error absoluto" = error_absoluto)
+
 
